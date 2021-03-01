b63593c30d3952a24d920d53d2a8f2a3e05c2d36
Rick Goshen <rick.goshen@gmail.com>
2021-02-28 17:42:51 -0700

    setup index and root route

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
+from flask import Flask, request, render_template, jsonify, session
 from boggle import Boggle
 
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "phillyflyersarechampions"
+
 boggle_game = Boggle()
+
+
+@app.route('/')
+def startpage():
+    """Show the game board."""
+
+    board = boggle_game.make_board()
+    session['board'] = board
+
+    return render_template("index.html")
